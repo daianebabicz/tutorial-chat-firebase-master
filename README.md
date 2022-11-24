39cd9bb5d01a211667bc1124c38164b3e9e9b503
Daiane Babicz <daibabicz@hotmail.com>
2022-11-24 13:36:54 -0300

    oi

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tutorial Chat Android Firebase
+
+Como criar um aplicativo de chat Android utilizando o recurso de Firebase
+
+Início da série: https://www.youtube.com/watch?v=wtUtt8AvU9M&list=PLJ0AcghBBWSjTa_4vWS8u_-0dvTgFyQis
+
+O Firebase Functions está no index.js
---
+++ app/src/main/java/com/example/chat_psicotinder/Notification.java
@@ -0,0 +1,20 @@
+package com.example.chat_psicotinder;
+
+/**
+ * Agosto, 02 2019
+ *
+ * @author suporte@moonjava.com.br (Tiago Aguiar).
+ */
+public class Notification extends Message {
+
+  private String fromName;
+
+  public String getFromName() {
+    return fromName;
+  }
+
+  public void setFromName(String fromName) {
+    this.fromName = fromName;
+  }
+
+}
